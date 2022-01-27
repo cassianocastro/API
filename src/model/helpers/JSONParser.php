4a148ee6b7841c23a493d9fd53478c8714e93793
cassianocastro <tottenham248@gmail.com>
2022-01-27 15:23:27 -0300

    Directory addition.

---
+++ src/model/helpers/JSONParser.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Models\Helpers;
+
+/**
+ *
+ */
+final class JSONParser
+{
+
+    public function encodeToJSON(array $array): string
+    {
+        return json_encode($array, JSON_PRETTY_PRINT);
+    }
+
+    public function decode(string $json, bool $toArray = true): object | array | null
+    {
+        return json_decode($json, $toArray);
+    }
+}
---
+++ src/model/helpers/Request.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Models\Helpers;
+
+/**
+ *
+ */
+final class Request
+{
+    public function __construct()
+    {
+        #$this->var = $var;
+    }
+}
