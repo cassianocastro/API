11a03a3672552d9dff6946c7c423639656d47636
Cassiano Castro <development.castro@gmail.com>
2024-02-20 23:09:48 -0300

    Removed debug function calls

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-ini_set("display_errors", true);
-ini_set("display_startup_errors", true);
-error_reporting(E_ALL);
-
-setlocale(LC_ALL, "", "pt_BR.utf8");
-
 require_once __DIR__ . '/vendor/autoload.php';
 
-$api = new Routes\Api();
-$api->loadMap();
-$api->setRequestAndDispatch();
+(new Api\Api())->receive(new Api\Http\Request());
