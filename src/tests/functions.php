d9655566e6e3da65aa9ad4fa096849d799bb1e3f
Cassiano Castro <development.castro@gmail.com>
2024-02-20 23:18:14 -0300

    Format adjust and test function definition

--- src/tests/functions.php
+++ src/tests/functions.php
@@ -1,48 +1,91 @@
 <?php
 
+/**
+ *
+ */
 function trace(string $txt): void
 {
-    if ( defined("APP_DEBUG") & APP_DEBUG ) {
+    if ( defined("APP_DEBUG") & APP_DEBUG )
+    {
         print $txt;
     }
 }
 
-if ( ! function_exists('getallheaders') ) {
-    /**
-    * Get all HTTP header key/values as an associative array for the current request.
-    * @return string[string] The HTTP header key/value pairs.
-    */
-    function getallheaders()
+/**
+ * Get all HTTP header key/values as an associative array for the current request.
+ * @return string[string] The HTTP header key/value pairs.
+ */
+function getallheaders(): array
+{
+    $headers     = [];
+
+    $copy_server = [
+        'CONTENT_TYPE'   => 'Content-Type',
+        'CONTENT_LENGTH' => 'Content-Length',
+        'CONTENT_MD5'    => 'Content-Md5',
+    ];
+
+    foreach ($_SERVER as $key => $value)
     {
-        $headers     = array();
-        $copy_server = array(
-            'CONTENT_TYPE'   => 'Content-Type',
-            'CONTENT_LENGTH' => 'Content-Length',
-            'CONTENT_MD5'    => 'Content-Md5',
-        );
+        if ( substr($key, 0, 5) === 'HTTP_' )
+        {
+            $key = substr($key, 5);
 
-        foreach ($_SERVER as $key => $value) {
-            if (substr($key, 0, 5) === 'HTTP_') {
-                $key = substr($key, 5);
-                if (!isset($copy_server[$key]) || !isset($_SERVER[$key])) {
-                    $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
-                    $headers[$key] = $value;
-                }
-            } elseif (isset($copy_server[$key])) {
-                $headers[$copy_server[$key]] = $value;
+            if ( ! isset($copy_server[$key]) or ! isset($_SERVER[$key]) )
+            {
+                $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
+                $headers[$key] = $value;
             }
         }
-
-        if ( ! isset($headers['Authorization']) ) {
-            if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
-                $headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
-            } elseif (isset($_SERVER['PHP_AUTH_USER'])) {
-                $basic_pass = $_SERVER['PHP_AUTH_PW'] ?? '';
-                $headers['Authorization'] = 'Basic ' . base64_encode("{$_SERVER['PHP_AUTH_USER']}:$basic_pass");
-            } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
-                $headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
-            }
+        elseif ( isset($copy_server[$key]) )
+        {
+            $headers[$copy_server[$key]] = $value;
         }
-        return $headers;
     }
+
+    if ( ! isset($headers['Authorization']) )
+    {
+        if ( isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION']) )
+            $headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+        elseif ( isset($_SERVER['PHP_AUTH_USER']) )
+        {
+            $basic_pass = $_SERVER['PHP_AUTH_PW'] ?? '';
+            $headers['Authorization'] = 'Basic ' . base64_encode("{$_SERVER['PHP_AUTH_USER']}:$basic_pass");
+        }
+        elseif (isset($_SERVER['PHP_AUTH_DIGEST']))
+            $headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
+    }
+
+    return $headers;
 }
+
+/**
+ * @test
+ */
+function canSendCsvFileToClient(): void
+{
+    $dataSet = [
+        [
+            "name" => "Test",
+            "age"  => 32
+        ],
+        [
+            "name" => "Test2",
+            "age"  => 40
+        ]
+    ];
+
+    header("Content-Type: text/csv; charset=UTF-8", false);
+    header("Content-Disposition: attachment; filename=test.csv", false);
+
+    $pointer = fopen("php://output", "w");
+
+    foreach ( $dataSet as $data )
+    {
+        fputcsv($pointer, $data);
+    }
+
+    fclose($pointer);
+}
+
+//canSendCsvFileToClient();
