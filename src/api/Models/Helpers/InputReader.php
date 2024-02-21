38d01fe1566f5f7ae0439bc9e60dd27a5f958a79
Cassiano Castro <development.castro@gmail.com>
2024-02-20 23:21:52 -0300

    Renamed class method and defined method readYAML()

--- src/api/Models/Helpers/InputReader.php
+++ src/api/Models/Helpers/InputReader.php
@@ -9,11 +9,18 @@ namespace Api\Models\Helpers;
 final class InputReader
 {
 
-    public function getData(string $path = "php://input"): array
+    public function readPhpInput(): array
     {
-        $content = file_get_contents($path);
+        $content = file_get_contents("php://input");
         $array   = (new JSONParser())->decode($content);
 
         return $array;
+    }
+
+    public function readYAML(string $filePath): array
+    {
+        $content = yaml_parse_file($filePath);
+
+        return $content;
     }
 }
