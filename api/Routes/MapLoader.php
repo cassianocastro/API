079c13472f2f477b9bf34e0b77d074b8e19464ef
Cassiano Castro <development.castro@gmail.com>
2024-02-23 17:46:30 -0300

    Adjusted names of imported classes

--- api/Routes/MapLoader.php
+++ api/Routes/MapLoader.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Api\Routes;
 
-use Api\Controllers\{ StudentsController, DisciplinesController };
+use Api\Services\{ StudentsService, DisciplinesService };
 use Api\Http\{ Request, Method };
 
 /**
@@ -22,31 +22,31 @@ final class MapLoader
     public function load(): void
     {
         $this->map->addRoute('/disciplines', Method::GET, function(Request $request) {
-            (new DisciplinesController())->get($request);
+            (new DisciplinesService())->get($request);
         });
 
         $this->map->addRoute('/disciplines/:i', Method::GET, function(Request $request) {
-            (new DisciplinesController())->get($request);
+            (new DisciplinesService())->get($request);
         });
 
         $this->map->addRoute('/students', Method::GET, function(Request $request) {
-            return (new StudentsController())->getStudents($request);
+            return (new StudentsService())->getStudents($request);
         });
 
 		$this->map->addRoute('/students', Method::POST, function(Request $request) {
-            return (new StudentsController())->post($request);
+            return (new StudentsService())->post($request);
         });
 
         $this->map->addRoute('/students/:i', Method::GET, function(Request $request) {
-            return (new StudentsController())->getStudentByID($request);
+            return (new StudentsService())->getStudentByID($request);
         });
 
         $this->map->addRoute('/students/:i', Method::PUT, function(Request $request) {
-            return (new StudentsController())->put($request);
+            return (new StudentsService())->put($request);
         });
 
         $this->map->addRoute('/students/:i', Method::DELETE, function(Request $request) {
-            return (new StudentsController())->delete($request);
+            return (new StudentsService())->delete($request);
         });
     }
 }
